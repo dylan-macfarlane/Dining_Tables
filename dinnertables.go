12affff9dcccc3b216de523091793c9c191dbf9b
dylan-macfarlane <59706407+dylan-macfarlane@users.noreply.github.com>
2020-02-11 11:33:32 -0800

    Added seating for days two and three

--- dinnertables.go
+++ dinnertables.go
@@ -1,4 +1,4 @@
-package main
+ package main
 
 import (
 	"bufio"
@@ -8,12 +8,20 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"strconv"
 	"time"
 )
 
+type Person struct {
+	Firstname string
+	Lastname  string
+	Table1    string
+	Table2    string
+	Table3    string
+}
+
+//var table declared globally because it is used in below func findTable()
 var table = 1
-var assignment = ""
-var waiterTable = 1
 
 func findTable() {
 	//find a new table assignment number
@@ -34,6 +42,16 @@ func contains(s []int, e int) bool {
 }
 
 func main() {
+
+	//Declaring variables that will be used throughout code.
+	var assignment = ""
+	//tablevar keeps track of how much each person changes tables between dinners
+	var tablevar = 1
+	//strings display table assignments at the end.
+	var table1disp = ""
+	var table2disp = ""
+	var table3disp = ""
+
 	//seeding so that it is random every time
 	rand.Seed(time.Now().UnixNano())
 
@@ -41,6 +59,7 @@ func main() {
 	tableFill := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	//usedTables keeps track of tables that are full
 	usedTables := []int{}
+	//studentList := []Person{}
 
 	csvFile, _ := os.Open("Dinner.csv")
 	reader := csv.NewReader(bufio.NewReader(csvFile))
@@ -64,42 +83,91 @@ func main() {
 				break
 			}
 		}
+		//sets tablevar at the specific person's order of seating in the table. For example, fifth person at a table gets a tableVar value of 5.
+		tablevar = tableFill[table] + 1
+		//loopvar is set to manually run the for loop three times.
+		var loopvar = 1
 
-		if table < 32 {
-			//continues to fill table until there are 8 people.
-			if tableFill[table] < 8 {
-				tableFill[table]++
-			} else {
-				tableFill[table]++
-				//once 8 people are at the table, adds one more and adds the table to usedTables to ignore.
-				usedTables = append(usedTables, table)
+		for {
+			if table < 32 {
+				//only runs on the first loop.
+				if loopvar == 1 {
+					//continues to fill table until there are 8 people.
+					if tableFill[table] < 8 {
+						tableFill[table]++
+					} else {
+						tableFill[table]++
+						//once 8 people are at the table, adds one more and adds the table to usedTables to ignore
+						usedTables = append(usedTables, table)
+					}
+				}
+				//this converts the int table to a string for assignment
+				assignment = strconv.Itoa(table)
+			} else if table == 32 {
+				//again only runs on the first loop
+				if loopvar == 1 {
+					//table 32 is kitchen crew, so same deal as above but with 6 people instead of 8.
+					if tableFill[table] < 6 {
+						tableFill[table]++
+					} else {
+						tableFill[table]++
+						//again, once hits 6 then add one more and close it off.
+						usedTables = append(usedTables, table)
+					}
+				}
+				//set assignment for Kitchen Crew
+				assignment = "Kitchen Crew"
+			} else if table == 33 {
+				//only runs in first loop again.
+				if loopvar == 1 {
+					//table 33 will be assigned as waiters.
+					if tableFill[table] < 30 {
+						tableFill[table]++
+					} else {
+						tableFill[table]++
+						//same deal, go until full.
+						usedTables = append(usedTables, table)
+					}
+				}
+				assignment = "Waiter"
+			}
+			//Each run-through of the loop sets the assignment for one night.
+			if loopvar == 1 {
+				table1disp = assignment
+			} else if loopvar == 2 {
+				table2disp = assignment
+			} else if loopvar == 3 {
+				table3disp = assignment
 			}
 
-			fmt.Println(line[0], line[1], table)
+			//move to the next table assignment and repeat loop.
+			table = table + tablevar
+			//make sure to avoid index out of range error.
+			for {
+				if table > 33 {
+					table = table - 33
+				} else {
+					break
+				}
+			}
 
-		} else if table == 32 {
-			//table 32 is kitchen crew, so same deal as above but with 6 people instead of 8.
-			if tableFill[table] < 6 {
-				tableFill[table]++
-			} else {
-				tableFill[table]++
-				//again, once hits 6 then add one more and close it off.
-				usedTables = append(usedTables, table)
+			//manually continue the loop
+			if loopvar < 3 {
+				loopvar++
+			} else if loopvar == 3 {
+				break
 			}
-			assignment = "Kitchen Crew"
-			fmt.Println(line[0], line[1], assignment)
-		} else if table == 33 {
-			//table 33 will be assigned as waiters.
-			if tableFill[table] < 30 {
-				tableFill[table]++
-			} else {
-				tableFill[table]++
-				//same deal, go until full.
-				usedTables = append(usedTables, table)
-			}
-			assignment = "Waiter"
-			waiterTable++
-			fmt.Println(line[0], line[1], assignment, waiterTable)
+
 		}
+		//Once all three nights are assigned, add specific person to struct Person
+		people := Person{
+			Firstname: line[1],
+			Lastname:  line[0],
+			Table1:    table1disp,
+			Table2:    table2disp,
+			Table3:    table3disp,
+		}
+		fmt.Println(people)
+
 	}
 }
